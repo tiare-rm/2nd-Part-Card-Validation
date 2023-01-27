ec49845490666ce171f7fc4fad8c2ce925af76f8
Tiare Rojas Madariaga <tiarerojasmadariaga_@hotmail.com>
2023-01-27 14:10:03 -0300

    hola

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
-import validator from './validator.js';
+import validator from "./validator.js";
 
-console.log(validator);
+//console.log(validator);
+
+const button = document.getElementById("botonvalidar"); //invoco al button del html para llamar la accion
+
+function isValid() {
+  const cardNumber = document.getElementById("cardnumber").value;
+ 
+  if (cardNumber === "" || isNaN(cardNumber) === true) {
+    alert("No ha ingresado número de tarjeta válida");
+  }
+  const valid = validator.isValid(cardNumber); //parametro de la funcion
+  if (valid === true) {
+    alert("Tarjeta válida, tu compra se está validando");
+  } else {
+    alert("Tarjeta Inválida");
+  }
+}
+
+function maskify() {
+  // funcion para enmascarar el numero
+  const cardnumber = document.getElementById("cardnumber").value;
+  const arrayMask = validator.maskify(cardnumber);
+  //console.log(arrayMask);
+  document.getElementById("cardnumber").value = arrayMask;
+}
+
+button.addEventListener("click", () => {
+  isValid();
+  maskify();
+});
+
+//document.getElementById("cardnumber").addEventListener("keypress", maskify); no lo usé al final
